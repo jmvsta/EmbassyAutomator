a86c6c88f4b8a3d8a09564d9c5db75b4dc49abd9
jmvsta <dmitry.bogdanov213@ya.ru>
2023-09-11 22:18:58 +0200

    some error handling corrections

--- src/main/kotlin/com/jmvsta/embassyautomator/EmbassyMonitor.kt
+++ src/main/kotlin/com/jmvsta/embassyautomator/EmbassyMonitor.kt
@@ -5,7 +5,6 @@ import jakarta.annotation.PreDestroy
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
@@ -14,19 +13,23 @@ import org.springframework.stereotype.Component
 
 @Component
 class EmbassyMonitor(
-    val telegramNotify: TelegramNotify,
+    private val telegramNotify: TelegramNotify,
     @Value("\${address1}") private val address1: String,
     @Value("\${address2}") private val address2: String,
     @Value("\${userEmail}") private val userEmail: String,
     @Value("\${userPassword}") private val userPassword: String,
     @Value("\${message}") private val message: String,
 ) {
-
     private val logger = LoggerFactory.getLogger(EmbassyMonitor::class.java)
-    private val driver: WebDriver = ChromeDriver(ChromeOptions().addArguments("--headless=chrome"))
+//    private val driver: WebDriver = ChromeDriver(ChromeOptions().addArguments("--headless=chrome"))
+    private val driver: WebDriver = ChromeDriver()
 
     @PostConstruct
     fun init() {
+        login()
+    }
+
+    private fun login() {
         driver.get("https://prenotami.esteri.it/Home")
         val email = driver.findElement(By.id("login-email"))
         email.sendKeys(userEmail)
@@ -35,23 +38,44 @@ class EmbassyMonitor(
         driver.findElement(By.id("login-form")).submit()
     }
 
+    private fun setLoggedIn(): Boolean {
+        if (driver.pageSource.contains("<title>Unavailable</title>")) {
+            logger.error("Overdudosed, login failed")
+            return false
+        } else {
+            try {
+                if (driver.findElement(By.id("login-email")).text.equals("")) {
+                    logger.error("Overdudosed, login failed")
+                    return false
+                }
+            } catch (e: Exception) {}
+            logger.info("Logged in to site")
+            return true
+        }
+    }
+
     @Scheduled(fixedRate = 300000)
     fun monitor() {
-        driver.get(address1)
-        logger.info("PAGE1 ${driver.pageSource}")
-        if (!driver.pageSource.contains(message)) {
-            logger.info("Time to book on $address1")
-            telegramNotify.sendNotification("Time to book on $address1")
+        if (setLoggedIn()) {
+            driver.get(address1)
+            logger.info("PAGE1 ${driver.pageSource}")
+
+            if (!driver.pageSource.contains(message)) {
+                logger.info("Time to book on $address1")
+                telegramNotify.sendNotification("Time to book on $address1")
+            } else {
+                logger.info("No bookings")
+            }
+            driver.get(address2)
+            logger.info("PAGE2 ${driver.pageSource}")
+            if (!driver.pageSource.contains(message)) {
+                logger.info("Place some logic $address2")
+                telegramNotify.sendNotification("Time to book on $address2")
+            } else {
+                logger.info("No bookings")
+            }
         } else {
-            logger.info("No bookings")
-        }
-        driver.get(address2)
-        logger.info("PAGE2 ${driver.pageSource}")
-        if (!driver.pageSource.contains(message)) {
-            logger.info("Place some logic $address2")
-            telegramNotify.sendNotification("Time to book on $address2")
-        } else {
-            logger.info("No bookings")
+            login()
         }
     }
 
